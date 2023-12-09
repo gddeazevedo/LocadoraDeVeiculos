cae713b52ca5912a1e878383ea3a26855b451f5d
Gabriel Dias <42484610+gddeazevedo@users.noreply.github.com>
2023-12-08 21:06:07 -0300

    implementing methods

--- src/util/ClienteValidator.java
+++ src/util/ClienteValidator.java
@@ -4,13 +4,13 @@ import exception.CNHException;
 import exception.CPFException;
 import exception.EmailException;
 
-import java.util.Date;
 import java.util.regex.Pattern;
 
 public class ClienteValidator {
     public static void validateCPF(String cpf) throws CPFException {
-
-//        throw new CPFException("CPF Invalido");
+        if (cpf.length() != 11) {
+            throw new CPFException("CPF Invalido");
+        }
     }
 
     public static void validateEmail(String email) throws EmailException {
@@ -22,10 +22,8 @@ public class ClienteValidator {
     }
 
     public static void validateCNH(String cnh) throws CNHException {
-//        throw new CNHException("CNH Invalido");
-    }
-
-    public static void validateValidadeCNH(Date validadeCNH) throws CNHException {
-//        throw new CNHException("Validade do CNH invalida!");
+        if (cnh.length() != 12) {
+            throw new CNHException("CNH invalido");
+        }
     }
 }
